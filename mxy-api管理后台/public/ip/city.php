092100ca7b3b3fae9abd7116b6a5f0e7b446c602
mengxinyuan <1648576390@qq.com>
2022-11-20 11:42:05 +0800

    v4.3

--- mxy-api管理后台/public/ip/city.php
+++
@@ -1,6 +0,0 @@
-<?php
-$msg = $_GET['msg'];
-if($msg==""){echo "抱歉，输入为空。";exit;}
-$str = file_get_contents("https://api.seniverse.com/v3/weather/now.json?key=SHSDTp4MYMHjrPgHb&location=$msg&language=zh-Hans&unit=c");
-echo $str;
-?>
--- mxy-api管理后台/public/ipa/key.php
+++
@@ -1,6 +0,0 @@
-<?php
-$aa=$_SERVER["HTTP_HOST"];
-$key="http://".$aa."";//网址
-$qq="1984873728";//QQ
-$mz="呵呵";//名称
-?>
